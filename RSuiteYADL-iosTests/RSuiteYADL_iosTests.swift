719e49bf7088e8b0e8e3e03f823aaad2be74689b
christinatsan <ctsango000@citymail.cuny.edu>
2017-06-13 10:52:17 -0400

    Initial Commit

---
+++ RSuiteYADL-iosTests/RSuiteYADL_iosTests.swift
@@ -0,0 +1,36 @@
+//
+//  RSuiteYADL_iosTests.swift
+//  RSuiteYADL-iosTests
+//
+//  Created by Christina Tsangouri on 6/13/17.
+//  Copyright © 2017 Christina Tsangouri. All rights reserved.
+//
+
+import XCTest
+@testable import RSuiteYADL_ios
+
+class RSuiteYADL_iosTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
